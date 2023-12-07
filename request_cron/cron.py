dfb98f267651701333f6449231adf67504f84b9d
Preetham Thava <tppreetham@gmail.com>
2023-12-07 07:37:13 +0530

    functions for cron job added

--- request_cron/cron.py
+++ request_cron/cron.py
@@ -1,59 +1,44 @@
+import json
 import os
-from flask import jsonify, Flask, request
+from flask import jsonify
+from redis import Redis
 from mysql.connector import Error
 import mysql.connector
-from redis import Redis
+import uuid
 import logging
 import datetime
 import time
 import atexit
+import requests
+from cassandra.cluster import Cluster
+
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.DEBUG)
 
-# app = Flask(__name__)
-
 # envs
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_A_HOST = os.getenv("DB_A_HOST", "localhost")
 DB_USERNAME = os.getenv("DB_USERNAME", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "rootpass")
-STORE_1 = os.getenv("STORE_1", "store_1")
-STORE_2 = os.getenv("STORE_2", "store_2")
-STORE_3 = os.getenv("STORE_3", "store_3")
+AUDIT = os.getenv("AUDIT", "order_audit")
 REQUEST_Q = "request"
 LOGGING_Q = "logging"
-# THRESHOLD_COUNT = 5
-
-connection1 = mysql.connector.connect(
-    host = DB_HOST,
-    user = DB_USERNAME,
-    password = DB_PASSWORD,
-    database = STORE_1
-    )
-
-connection2 = mysql.connector.connect(
-    host = DB_HOST,
-    user = DB_USERNAME,
-    password = DB_PASSWORD,
-    database = STORE_2
-    )
-
-connection3 = mysql.connector.connect(
-    host = DB_HOST,
-    user = DB_USERNAME,
-    password = DB_PASSWORD,
-    database = STORE_3
-    )
-
 
 queue = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
-print(connection1)
-
+def printDebugOutput(response):
+    s = response.text
+    try:
+        j = json.loads(s)
+        print("Response is", response)
+        print(j)
+    except json.JSONDecodeError:
+        return response
+    
 def fetch_requests():
     # Fetch reqs from req queue 
     # req fmt: station_num, item, item_req_cnt
@@ -68,97 +53,152 @@ def fetch_requests():
         else:
             req_dict[item] = count
         station_reqs.append(r)
+    print("station_reqs, req_dict")
     print(station_reqs, req_dict)
     return station_reqs, req_dict
-    # pass    
+    
+# data = [('store_1', 'apple', 10, 9.08), ('store_1', 'banana', 10, 5.0), ('store_2', 'orange', 10, 8.0), 
+#         ('store_2', 'milk', 1, 15.0), ('store_3', 'eggs', 10, 3.8), ('store_3', 'apples', 10, 3.8), 
+#         ('store_3', 'cereal', 1, 2.5)]
 
-def process_requests(req_dict):
-    # Do cost optimization orders here and place on store DB's
-    # the final order sent to process_req must have unique items only
-    data = fetch_items_from_all_stores()
-    print(data)
-    # pass
+# orders = [("apples", 10), ("banana", 6)]
+
+def optimize_order(data, orders):
+    # Create a dictionary to store the optimized order
+    stores_len = 3
+    stores_map = {"store_1": 0, "store_2": 1, "store_3": 2}
+    order_stores = [[] for i in range(stores_len)]
+
+    # Iterate through each order
+    for item, quantity_requested in orders:
+        # Check if the item is available in the data
+        if any(entry[1] == item for entry in data):
+            # Filter stores that have the requested item in their inventory
+            available_stores = [entry for entry in data if entry[1] == item]
+
+            # Sort stores based on cost per item
+            available_stores.sort(key=lambda x: x[3])
+
+            # Iterate through available stores to find the lowest cost
+            for entry in available_stores:
+                store, _, quantity_available, cost = entry
+
+                # Check if the store has enough quantity
+                if quantity_requested <= quantity_available:
+                    # Update the optimized order
+                    order_stores[stores_map[store]].append((item, quantity_requested))
+                    quantity_requested-=quantity_requested
+                    break
+                else:
+                    order_stores[stores_map[store]].append((item, quantity_available))
+                    quantity_requested-=quantity_available
+        if quantity_requested != 0:
+            print("The store isnt able to satisfy the given request.")
+
+    return order_stores
 
 def fetch_items_from_all_stores():
-    store_invtry = []
-    sql = "SELECT * from store"
-    # store fmt: id, item, qty, price
-    store_s = "store_1"
-    try:
-        cursor = connection1.cursor()
-    except:
-        return f"Could not connect to {store_s}"
+    host = "localhost"
+    port = 3000
+    addr = f"http://{host}:{port}"
+    url = addr + f"/items"
+    response = requests.get(url)
+    print(response, type(response))
+    if response.ok:
+        printDebugOutput(response)
+    else:
+        return "Some error occurred in fetch_items_from_all_stores"
+    return json.loads(response.text)
     
-    try:
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
-        if result and len(result)>0:
-            for rec in result:
-                lt = list(rec)
-                lt[0] = store_s
-                store_invtry.append(tuple(lt))
+def process_requests(data, req_dict):
+    # Do cost optimization orders here and place on store DB's
+    # the final order sent to process_req must have unique items only
+    orders = []
+    for item, qty in req_dict.items():
+        orders.append((item, qty))
+    print("orders")
+    print(orders)
+    optimized_order = optimize_order(data, orders)
+    print("optimized_order")
+    print(optimized_order)
+    return optimized_order
+    # pass
 
-    except Exception as e:
-        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
+def place_order_on_stores(order_stores):    
+    host = "localhost"
+    store_port = 3000
+    addr = f"http://{host}:{store_port}"
+    order_bills = {}
+    order_bills["1"] = []
+    order_bills["2"] = []
+    order_bills["3"] = []
+    for store_num, order in enumerate(order_stores):
+        if len(order)!=0:
+            url = addr + f"/order/{store_num+1}"
+            response = requests.post(url, json=json.dumps(order))
+            if response.ok:
+                printDebugOutput(response)
+                resp = json.loads(response.text)
+                order_bills[str(store_num+1)] = (resp["order_bill"], resp["order_total"])
+            else:
+                print(response.status_code, response.text)
+                print("Some error occurred on place_order_on_stores")
+    return order_bills
+
+def gen_uuid():
+    return uuid.UUID()
+
+def add_final_order_onto_audit_db(order_bills):
+    # Add the final order obtained with price onto audit DB
     
-    store_s = "store_2"
-    try:
-        cursor = connection2.cursor()
-    except:
-        return f"Could not connect to {store_s}"
-    
-    try:
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
-        if result and len(result)>0:
-            for rec in result:
-                lt = list(rec)
-                lt[0] = store_s
-                store_invtry.append(tuple(lt))
-
-    except Exception as e:
-        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
-
-    store_s = "store_3"    
-    try:
-        cursor = connection3.cursor()
-    except:
-        return f"Could not connect to {store_s}"
-    
-    try:
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
-        if result and len(result)>0:
-            for rec in result:
-                lt = list(rec)
-                lt[0] = store_s
-                store_invtry.append(tuple(lt))
-
-    except Exception as e:
-        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
-    print(store_invtry)
-    return store_invtry
-
-
-
-def add_final_order_onto_audit_db():
-    # Add the final order obtained with price onto audit DB - cassandra
+    # cur.execute("create table orders(id INT PRIMARY KEY AUTO_INCREMENT, store VARCHAR(25), total float);")
+    # cur.execute('create table order_items(id INT PRIMARY KEY AUTO_INCREMENT, order_id int, item varchar(255), quantity int, price float, total_price float, FOREIGN KEY (order_id) REFERENCES orders(id));')
+    # for 
+    # stmt = session.prepare("
+    #     INSERT INTO orders (rowkey, qualifier, info, act_date, log_time)
+    #     VALUES (?, ?, ?, ?, ?)
+    #     IF NOT EXISTS
+    #     ")
+    # results = session.execute(stmt, [arg1, arg2, ...])
     pass
+    for k, v in order_bills.items():
+        if v!=[]:
+            items, total = v
+            stmt = session.prepare("INSERT INTO orders(id, store, items, total) values ({}, {}, {}, {})")
+            stmt = "BEGIN BATCH"
+            # stmt += "APPLY BATCH"
+            for i in items:
+                stmt += f"INSERT INTO order_items(id, order_id, )"
+
+
+
+
+    
 
 def send_satisfied_req_on_q():
     pass
     # Send the final order back on the q, for restock worker to handle
 
-
 def cron_job():
     full_reqs, req_dict = fetch_requests()
-    process_requests(req_dict)
+    store_inventory = fetch_items_from_all_stores()
+    final_order = process_requests(store_inventory, req_dict)
+    # format of final order: [[], [], []] each list inside the big list is the order for that store with the index of the list
+    order_bills = place_order_on_stores(final_order)
+    print(order_bills)
+    add_final_order_onto_audit_db(order_bills)
+    # send_satisfied_req_on_q(final_order, full_reqs)
+
+
       
 if __name__ == "__main__":
     cron_job()
+    cluster = Cluster(['0.0.0.0'],port=9042)
+    session = cluster.connect('order_audit',wait_for_all_pools=True)
+    session.execute('USE order_audit')
+    # rows = session.execute('SELECT * FROM orders')
+    # for row in rows:
+    #     print(row.id,row.total,row.items, row.store)
     # try:
     #     cur1 = connection1.cursor()
     #     cur2 = connection2.cursor()
@@ -166,7 +206,6 @@ if __name__ == "__main__":
     # except:
     #     Exception("Couldn't connect to the station DBs")
 
-    # app.run(host="0.0.0.0", port=5000)
 
 
 
---
+++ request_cron/db_server.py
@@ -0,0 +1,294 @@
+import json
+import os
+from flask import jsonify, Flask, request
+from mysql.connector import Error
+import mysql.connector
+from redis import Redis
+import logging
+import datetime
+import time
+import atexit
+import requests
+
+# envs
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
+
+# envs
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+DB1_HOST = os.getenv("DB1_HOST", "localhost")
+DB2_HOST = os.getenv("DB2_HOST", "localhost")
+DB3_HOST = os.getenv("DB3_HOST", "localhost")
+DB_USERNAME = os.getenv("DB_USERNAME", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "rootpass")
+STORE_1 = os.getenv("STORE_1", "store_1")
+STORE_2 = os.getenv("STORE_2", "store_2")
+STORE_3 = os.getenv("STORE_3", "store_3")
+REQUEST_Q = "request"
+LOGGING_Q = "logging"
+THRESHOLD_COUNT = 10
+
+app = Flask(__name__)
+
+# Creating and initializing store DB's
+try: 
+    connection1 = mysql.connector.connect(
+        host = DB1_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_1,
+        # port = 3305
+        )
+except:
+    connection1 = mysql.connector.connect(
+        host = DB1_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        # port = 3305
+        )
+    cur = connection1.cursor()
+    cur.execute("CREATE DATABASE store_1")
+    connection1 = mysql.connector.connect(
+        host = DB1_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_1,
+        # port = 3305
+        )
+    cur = connection1.cursor()
+    cur.execute("CREATE TABLE store(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), quantity INT, price float);")
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("apple", 10, 9.18);')
+    connection1.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("banana", 1, 5);')
+    connection1.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("mango", 3, 2.35);')
+    connection1.commit()
+
+try: 
+    connection2 = mysql.connector.connect(
+        host = DB2_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_2,
+        # port = 4305
+        )
+except:
+    connection2 = mysql.connector.connect(
+        host = DB2_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        # port = 4305
+        )
+    cur = connection2.cursor()
+    cur.execute("CREATE DATABASE store_2")
+    connection2 = mysql.connector.connect(
+        host = DB2_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_2,
+        # port = 4305
+        )
+    cur = connection2.cursor()
+    cur.execute("CREATE TABLE store(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), quantity INT, price float);")
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("orange", 10, 8);')
+    connection2.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("milk", 1, 15);')
+    connection2.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("eggs", 3, 2);')
+    connection2.commit()
+
+try: 
+    connection3 = mysql.connector.connect(
+        host = DB3_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_3,
+        # port = 5305
+        )
+except:
+    connection3 = mysql.connector.connect(
+        host = DB3_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        # port = 5305
+        )
+    cur = connection3.cursor()
+    cur.execute("CREATE DATABASE store_3")
+    connection3 = mysql.connector.connect(
+        host = DB3_HOST,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        database = STORE_3,
+        # port = 5305
+        )
+    cur = connection3.cursor()
+    cur.execute("CREATE TABLE store(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), quantity INT, price float);")
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("eggs", 10, 3.80);')
+    connection3.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("apples", 15, 4.23);')
+    connection3.commit()
+    cur.execute('INSERT INTO store(name, quantity, price) VALUES("cereal", 3, 5.12);')
+    connection3.commit()
+
+def printDebugOutput(response):
+    s = response.text
+    try:
+        j = json.loads(s)
+        print("Response is", response)
+        print(j)
+    except json.JSONDecodeError:
+        return response
+
+@app.route("/items", methods=["GET"])
+def fetch_all_items():
+    store_invtry = []
+    sql = "SELECT * from store"
+    # store fmt: id, item, qty, price
+    store_s = "store_1"
+    try:
+        cursor = connection1.cursor()
+    except:
+        return f"Could not connect to {store_s}"
+    
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(f"result from {store_s}")
+        print(result)
+        if result and len(result)>0:
+            for rec in result:
+                lt = list(rec)
+                lt[0] = store_s
+                store_invtry.append(tuple(lt))
+
+    except Exception as e:
+        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
+    
+    store_s = "store_2"
+    try:
+        cursor = connection2.cursor()
+    except:
+        return f"Could not connect to {store_s}"
+    
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(f"result from {store_s}")
+        print(result)
+        if result and len(result)>0:
+            for rec in result:
+                lt = list(rec)
+                lt[0] = store_s
+                store_invtry.append(tuple(lt))
+
+    except Exception as e:
+        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
+
+    store_s = "store_3"    
+    try:
+        cursor = connection3.cursor()
+    except:
+        return f"Could not connect to {store_s}"
+    
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(f"result from {store_s}")
+        print(result)
+        if result and len(result)>0:
+            for rec in result:
+                lt = list(rec)
+                lt[0] = store_s
+                store_invtry.append(tuple(lt))
+
+    except Exception as e:
+        return f"Exception occurred while fetching items from {store_s}: {str(e)}"
+    print(store_invtry)
+    return jsonify(store_invtry)
+
+@app.route("/order/<int:store_num>", methods=["POST"])
+def order_items(store_num):
+    """ Places the order to the specified store"""
+    order_json = request.get_json()
+    # example format: {'apples': 10, 'banana': 20}
+    print(order_json)
+    order_list = json.loads(order_json)
+
+    # return order_json
+    # log.debug(f"[{datetime.datetime.now()}] Recieved request for {request.url} with data {(station_num, item, count)}")
+    order_bill = []
+    order_total = 0.0
+    if store_num not in [1, 2, 3]:
+        return jsonify("wrong store number!")
+    
+    try:
+        cursor = connection1.cursor()
+        if store_num == 2:
+            cursor = connection2.cursor()
+        elif store_num == 3:
+            cursor = connection3.cursor()
+    except:
+        return jsonify("not connected to db")
+    
+    for item, qty in order_list:
+
+        sql = f"SELECT quantity, price FROM store WHERE name = '{item}'"
+        try:
+            print(sql)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            # print(result)
+
+            if not result:
+                print(item, qty)
+                resp = jsonify("the requested item doesn't exist in the store")
+                resp.status_code = 404
+                return resp
+
+            print(result[0])
+            item_qty = result[0]
+            item_qty_to_set = item_qty - qty
+            if item_qty - qty < THRESHOLD_COUNT:
+                item_qty_to_set = THRESHOLD_COUNT
+            sql = f"UPDATE store SET quantity = {item_qty_to_set} WHERE name = '{item}'"
+            cursor.execute(sql)
+            connection1.commit()
+
+            # queue.rpush(LOGGING_Q, f"[{datetime.datetime.now()}] added {count} {item}s in station {station_num} completed and the current count is {item_count + count}")
+            price = result[1]
+            order_total+=(qty*price)
+            order_bill.append((item, qty, price, qty*price))
+        
+        except Exception as exception:
+            resp = jsonify(f"couldn't process the request - {str(exception)}")
+            resp.status_code = 400
+            return resp
+
+    resp = jsonify({
+        "status": "request processed",
+        "order_total": order_total,
+        "order_bill": order_bill
+    })
+
+    resp.status_code = 200
+    return resp
+    
+# store_s = "store_1"
+# host = "localhost"
+# addr = f"http://{host}:3000"
+# url = addr + f"/items"
+# response = requests.post(url, json={"apples": 10, "banana": 20})
+# if response.ok:
+#     printDebugOutput(response)
+
+
+if __name__ == "__main__":
+    try:
+        cur1 = connection1.cursor()
+        cur2 = connection2.cursor()
+        cur3 = connection3.cursor()
+    except:
+        Exception("Couldn't connect to the station DBs")
+    app.run(host="0.0.0.0", port=3000)
+
